4a51dacfe5d613b8d91b0d2c398a6c814bba5ed4
rf17017 <125318173+rf17017@users.noreply.github.com>
2023-04-03 10:21:36 +1200

    Flask installation finished, template runs

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask import render_template
+
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/home")
 def home():
-    return "Hello, Flask!"
+    return render_template("skadoosh.html")
+
