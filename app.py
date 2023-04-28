21f2ebb069b2d755bb807377632b49cfd2c36905
rf17017 <125318173+rf17017@users.noreply.github.com>
2023-04-28 18:20:25 +1200

    Homepage design finished CSS stylesheet linked in main python file *reusable* added and linked Background image for basic pages from now on Changed styles of buttons to add margins, stopped buttons touching Turned on debug mode and auto run so no more typing "python -m flask run" Added a static filepath folder for future design changes "possible light/dark mode feature"

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask import render_template
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route("/home")
 def home():
     return render_template("skadoosh.html")
 
+if __name__ == '__main__':
+    app.run(debug=True)
